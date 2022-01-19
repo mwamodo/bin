0c3b7991da4d1dd23f2b7d5213de8874177cca5e
mwamodo <mwamodoinfo@gmail.com>
2022-01-19 05:45:58 +0300

    bin, initial commit

---
+++ init/exports.sh
@@ -0,0 +1,46 @@
+# npm global path to prevent install npm with sudo
+NPM_PACKAGES="${HOME}/.npm-packages"
+
+# Insert globally installed npm packages to path and local bin
+export PATH="$PATH:$NPM_PACKAGES/bin"
+
+# Unset manpath so we can inherit from /etc/manpath via the `manpath` command
+export MANPATH="${MANPATH-$(manpath)}:$NPM_PACKAGES/share/man"
+
+export DATE=`date +%d-%m-%Y`
+
+# insert bin/scripts directory to path
+export PATH=$PATH:$HOME/bin/scripts
+
+# PHP
+export PATH="$PATH:$HOME/.composer/vendor/bin"
+export XDEBUG_MODE=coverage
+
+# Git and GitHub
+commit () {
+    commitMessage="$1"
+
+    if [ "$commitMessage" = "" ]
+    then
+        commitMessage="WIP"
+    fi
+
+    git add .
+    eval "git commit -a -m '${commitMessage}'"
+}
+
+commit:push () {
+
+    commitMessage="$1"
+    gitCurrentBranch=$(git branch --show-current)
+
+    if [ "$commitMessage" = "" ]
+    then
+        commitMessage="WIP"
+    fi
+
+    git add .
+    eval "git commit -a -m '${commitMessage}'"
+    eval "git push origin ${gitCurrentBranch}"
+
+}
