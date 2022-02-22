39d29124a731489d885bb02a3e005c448d297f16
Mwamodo Erick <mwamodoinfo@gmail.com>
2022-02-22 12:15:27 +0300

    added nvm

--- init/exports.sh
+++ init/exports.sh
@@ -23,6 +23,10 @@ export XDEBUG_MODE=coverage
 # Flutter
 export PATH="$PATH:$HOME/Code/Externals/flutter/bin"
 
+# This loads nvm
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
 # Git and GitHub
 commit () {
     commitMessage="$1"
