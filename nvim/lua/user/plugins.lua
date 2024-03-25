b7675a7869cda20b258dd9ae470ab56fdcd6713e
Erick Mwamodo <erick@mwamodo.com>
2024-03-25 08:28:57 +0300

    WIP

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -1,30 +1,51 @@
 local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+    local fn = vim.fn
+    local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+        vim.cmd [[packadd packer.nvim]]
+        return true
+    end
+    return false
 end
 
 local packer_bootstrap = ensure_packer()
 
 require('packer').reset()
 require('packer').init({
-  compile_path = vim.fn.stdpath('data')..'/site/plugin/packer_compiled.lua',
-  --display = {
-  --  open_fn = function()
-  --    return require('packer.util').float({ border = 'solid' })
-  --  end,
-  --},
+    compile_path = vim.fn.stdpath('data') .. '/site/plugin/packer_compiled.lua',
+    display = {
+        open_fn = function()
+            return require('packer.util').float({ border = 'solid' })
+        end,
+    },
 })
 
 local use = require('packer').use
 
 -- Packer can manage itself.
 use('wbthomason/packer.nvim')
+
+-- One Dark theme.
+use({
+    'jessarcher/onedark.nvim',
+    config = function()
+        vim.cmd('colorscheme onedark')
+
+        vim.api.nvim_set_hl(0, 'FloatBorder', {
+            fg = vim.api.nvim_get_hl_by_name('NormalFloat', true).background,
+            bg = vim.api.nvim_get_hl_by_name('NormalFloat', true).background,
+        })
+
+        -- Make the cursor line background invisible
+        vim.api.nvim_set_hl(0, 'CursorLineBg', {
+            fg = vim.api.nvim_get_hl_by_name('CursorLine', true).background,
+            bg = vim.api.nvim_get_hl_by_name('CursorLine', true).background,
+        })
+
+        vim.api.nvim_set_hl(0, 'NvimTreeIndentMarker', { fg = '#30323E' })
+    end,
+})
 
 -- Commenting support.
 use('tpope/vim-commentary')
@@ -61,61 +82,84 @@ use('jessarcher/vim-heritage')
 
 -- Text objects for HTML attributes.
 use({
-  'whatyouhide/vim-textobj-xmlattr',
-  requires = {'kana/vim-textobj-user'},
+    'whatyouhide/vim-textobj-xmlattr',
+    requires = { 'kana/vim-textobj-user' },
 })
 
 -- Automatically set the working directory to the project root.
 use({
-  'airblade/vim-rooter',
-  setup = function()
-    -- Instead of this running every time we open a file, we'll just run it once when Vim starts.
-    vim.g.rooter_manual_only = 1
-  end,
-  config = function()
-    vim.cmd('Rooter')
-  end,
+    'airblade/vim-rooter',
+    setup = function()
+        -- Instead of this running every time we open a file, we'll just run it once when Vim starts.
+        vim.g.rooter_manual_only = 1
+    end,
+    config = function()
+        vim.cmd('Rooter')
+    end,
 })
 
 -- Automatically add closing brackets, quotes, etc.
 use({
-  'windwp/nvim-autopairs',
-  config = function()
-    require('nvim-autopairs').setup()
-  end,
+    'windwp/nvim-autopairs',
+    config = function()
+        require('nvim-autopairs').setup()
+    end,
 })
 
 -- Add smooth scrolling to avoid jarring jumps
 use({
-  'karb94/neoscroll.nvim',
-  config = function()
-    require('neoscroll').setup()
-  end,
+    'karb94/neoscroll.nvim',
+    config = function()
+        require('neoscroll').setup()
+    end,
 })
 
 -- All closing buffers without closing the split window.
 use({
-  'famiu/bufdelete.nvim',
-  config = function()
-    vim.keymap.set('n', '<Leader>q', ':Bdelete<CR>')
-  end,
+    'famiu/bufdelete.nvim',
+    config = function()
+        vim.keymap.set('n', '<Leader>q', ':Bdelete<CR>')
+    end,
 })
 
 -- Split arrays and methods onto multiple lines, or join them back up.
 use({
-  'AndrewRadev/splitjoin.vim',
-  config = function()
-    vim.g.splitjoin_html_attributes_bracket_on_new_line = 1
-    vim.g.splitjoin_trailing_comma = 1
-    vim.g.splitjoin_php_method_chain_full = 1
-  end,
+    'AndrewRadev/splitjoin.vim',
+    config = function()
+        vim.g.splitjoin_html_attributes_bracket_on_new_line = 1
+        vim.g.splitjoin_trailing_comma = 1
+        vim.g.splitjoin_php_method_chain_full = 1
+    end,
 })
 
 -- Automatically fix indentation when pasting code.
 use({
-  'sickill/vim-pasta',
+    'sickill/vim-pasta',
+    config = function()
+        vim.g.pasta_disabled_filetypes = { 'fugitive' }
+    end,
+})
+
+-- Fuzzy finder
+use({
+    'nvim-telescope/telescope.nvim',
+    requires = {
+        { 'nvim-lua/plenary.nvim' },
+        { 'kyazdani42/nvim-web-devicons' },
+        { 'nvim-telescope/telescope-live-grep-args.nvim' },
+        { 'nvim-telescope/telescope-fzf-native.nvim',    run = 'make' },
+    },
+    config = function()
+        require('user/plugins/telescope')
+    end,
+})
+
+-- File tree sidebar
+use({
+  'kyazdani42/nvim-tree.lua',
+  requires = { 'kyazdani42/nvim-web-devicons' },
   config = function()
-    vim.g.pasta_disabled_filetypes = { 'fugitive' }
+    require('user/plugins/nvim-tree')
   end,
 })
 
