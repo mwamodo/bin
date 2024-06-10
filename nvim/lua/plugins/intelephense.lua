de3066d8f1da077e36710b754573ce8fdd4067bd
Erick Mwamodo <erick@mwamodo.com>
2024-06-10 06:30:00 +0300

    WIP

---
+++ nvim/lua/plugins/intelephense.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        intelephense = {
+          filetypes = { "php", "blade" },
+          settings = {
+            intelephense = {
+              filetypes = { "php", "blade" },
+              files = {
+                associations = { "*.php", "*.blade.php" }, -- Associating .blade.php files as well
+                maxSize = 5000000,
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+}
