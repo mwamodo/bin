0b903719a1fb1cc56f3dfb801f98551e9dd6d94e
Erick Mwamodo <erick@mwamodo.com>
2024-10-19 03:48:50 +0300

    nvim laravel configurations

---
+++ nvim/lua/plugins/mason.lua
@@ -0,0 +1,12 @@
+return {
+  "williamboman/mason.nvim",
+  opts = {
+    ensure_installed = {
+      "blade-formatter",
+      "intelephense",
+      "tailwindcss-language-server",
+      "pint",
+      "rustywind",
+    },
+  },
+}
