681901d271a23d512c91d4e893b8d8f1077baf3d
Erick Mwamodo <erick@mwamodo.com>
2024-12-01 11:19:08 +0300

    WIP

--- nvim/init.lua
+++ nvim/init.lua
@@ -99,22 +99,10 @@ require 'options'
 -- [[ Basic Keymaps ]]
 require 'keymaps'
 
--- [[ Basic Autocommands ]]
---  See `:help lua-guide-autocommands`
-
--- Highlight when yanking (copying) text
---  Try it with `yap` in normal mode
---  See `:help vim.highlight.on_yank()`
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-})
-
+-- [[ Install `lazy.nvim` plugin manager ]]
 require 'lazy-bootstrap'
 
+-- [[ Configure and install plugins ]]
 require 'lazy-plugins'
 
 -- The line beneath this is called `modeline`. See `:help modeline`
