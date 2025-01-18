ec1ba6a6c66155bf1c1a988dc51cdd9b38d40a9a
Erick Mwamodo <erick@mwamodo.com>
2025-01-19 01:21:55 +0300

    better tab names for warp terminal

--- zsh/exports.sh
+++ zsh/exports.sh
@@ -93,3 +93,14 @@ lab() {
         ssh -tt mwamodo@${HOME_IP} "bash -ic '$*'"
     fi
 }
+
+# sets the tab name for warp to the current directory name
+set_name () {
+  echo -ne "\033]0;${PWD##*/}\007"
+}
+
+if [ -n "$ZSH_VERSION" ]; then
+  precmd_functions+=(set_name)
+elif [ -n "$BASH_VERSION" ]; then
+  PROMPT_COMMAND='set_name'
+fi
