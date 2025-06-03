5ee10fbb559f03403dd90578d2090da727fb5f93
Erick Mwamodo <erick@mwamodo.com>
2025-06-03 12:37:09 +0300

    flexoki neovim theme

--- nvim/lua/kickstart/plugins/flexoki.lua
+++ nvim/lua/kickstart/plugins/flexoki.lua
@@ -1,3 +1,27 @@
 return {
-    { 'kepano/flexoki-neovim', name = 'flexoki' }
+    { 'kepano/flexoki-neovim', name = 'flexoki' },
+    {
+        'neanias/everforest-nvim',
+        priority = 1000,
+        config = function()
+            -- Function to set the appropriate theme based on system appearance
+            local function set_theme()
+                local appearance = vim.fn.system('defaults read -g AppleInterfaceStyle'):gsub('%s+', '')
+                if appearance == 'Dark' then
+                    vim.cmd.colorscheme 'flexoki-dark'
+                else
+                    vim.cmd.colorscheme 'flexoki-light'
+                end
+            end
+
+            -- Set initial theme
+            set_theme()
+
+            -- Watch for system appearance changes
+            vim.api.nvim_create_autocmd('Signal', {
+                pattern = 'SIGUSR1',
+                callback = set_theme
+            })
+        end,
+    }
 }
