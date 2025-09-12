dcc5d2c18b5ab243f9cfb1dd8d99081918a62f92
Erick Mwamodo <erick@mwamodo.com>
2025-09-12 09:59:15 +0300

    Enhance WezTerm configuration for improved aesthetics and usability
    - Added window decorations and adjusted tab bar settings for a cleaner interface.
    - Configured window frame properties including font and background colors.
    - Defined color scheme for active, inactive, and new tabs to enhance visibility.

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -8,5 +8,34 @@ config.font = wezterm.font('Dank Mono')
 config.font_size = 18
 
 config.window_background_opacity = 0.9
+config.window_decorations = 'RESIZE'
+
+config.hide_tab_bar_if_only_one_tab = true
+config.use_fancy_tab_bar = false
+
+config.window_frame = {
+    font = wezterm.font { family = 'Dank Mono', weight = 'Bold' },
+    font_size = 15,
+
+    active_titlebar_bg = '#1e1e2e',
+    inactive_titlebar_bg = '#1e1e2e',
+}
+
+config.colors = {
+    tab_bar = {
+        active_tab = {
+            bg_color = '#1e1e2e',
+            fg_color = '#cdd6f4'
+        },
+        inactive_tab = {
+            bg_color = '#181825',
+            fg_color = '#cdd6f4',
+        },
+        new_tab = {
+            bg_color = '#1e1e2e',
+            fg_color = '#cdd6f4',
+        }
+    }
+}
 
 return config
