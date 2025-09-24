ecf54ba283553e102b4d243b0c9f19aa22dafe55
Erick Mwamodo <erick@mwamodo.com>
2025-09-24 14:07:09 +0300

    increase wezterm font

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -49,7 +49,7 @@ local function tabbar_for_appearance(appearance)
 end
 
 config.font = wezterm.font("Dank Mono")
-config.font_size = 18
+config.font_size = 25
 
 config.window_background_opacity = 1
 config.window_decorations = "RESIZE"
