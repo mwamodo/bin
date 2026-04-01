a9a5334f2de1b0e987201a108b05ca2d1f0ef11a
Erick Mwamodo <erick@mwamodo.com>
2026-04-02 02:59:26 +0300

    WIP

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -58,6 +58,8 @@ config.window_decorations = "RESIZE"
 config.hide_tab_bar_if_only_one_tab = true
 config.use_fancy_tab_bar = false
 
+config.enable_kitty_keyboard = true
+
 do
 	local appearance = wezterm.gui.get_appearance()
 	config.color_scheme = scheme_for_appearance(appearance)
