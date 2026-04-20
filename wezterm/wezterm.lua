643eae065ceff2cf120de4162634689915854b55
Erick Mwamodo <erick@mwamodo.com>
2026-04-20 16:21:52 +0300

    chore(dotfiles): refresh tmux and wezterm configs
    - migrate tmux to the new reset/plugin-based layout and restore familiar bindings
    - add tmux session, split, reload, and calendar status updates
    - remove old flexoki tmux themes and include the TPM plugin tree

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -10,7 +10,7 @@ return {
 	-- macos_window_background_blur = 40,
 	macos_window_background_blur = 30,
 
-	-- window_background_image = '/Users/omerhamerman/Downloads/3840x1080-Wallpaper-041.jpg',
+	-- window_background_image = '/Users/mwamodo/Downloads/3840x1080-Wallpaper-041.jpg',
 	-- window_background_image_hsb = {
 	-- 	brightness = 0.01,
 	-- 	hue = 1.0,
@@ -34,7 +34,6 @@ return {
 		},
 	},
 	mouse_bindings = {
-	  -- Ctrl-click will open the link under the mouse cursor
 	  {
 	    event = { Up = { streak = 1, button = 'Left' } },
 	    mods = 'CTRL',
