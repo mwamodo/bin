a97bac71cacb2b6a64eb234b26f0a09f455b6bcc
Erick Mwamodo <erick@mwamodo.com>
2026-04-20 19:13:36 +0300

    chore(wezterm): use Monaspace Krypton font

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,7 +6,7 @@ return {
 	color_scheme = 'Catppuccin Mocha',
 	enable_tab_bar = false,
 	font_size = 16.0,
-	font = wezterm.font('JetBrains Mono'),
+	font = wezterm.font('Monaspace krypton'),
 	-- macos_window_background_blur = 40,
 	macos_window_background_blur = 30,
 
