73afea44afe623d6de47ebc05681a44792947df8
Erick Mwamodo <erick@mwamodo.com>
2026-04-30 07:24:07 +0300

    WIP

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,21 +2,14 @@ local wezterm = require 'wezterm'
 
 return {
 	adjust_window_size_when_changing_font_size = false,
-	-- color_scheme = 'termnial.sexy',
 	color_scheme = 'Catppuccin Mocha',
 	enable_tab_bar = false,
 	font_size = 18.0,
 	font = wezterm.font('Monaspace krypton'),
-	-- macos_window_background_blur = 40,
-	macos_window_background_blur = 30,
 
-	--window_background_image = '/Users/mwamodo/Pictures/moon.jpg',
-	-- window_background_image_hsb = {
-	--	brightness = 0.10,
-	-- 	hue = 1.0,
-	-- 	saturation = 0.5,
-	--},
-	window_background_opacity = 0.52,
+	-- macos_window_background_blur = 50,
+	-- window_background_opacity = 0.90,
+
 	window_decorations = 'RESIZE',
 	keys = {
 		{
@@ -25,7 +18,7 @@ return {
 			action = wezterm.action.ToggleFullScreen,
 		},
 		{
-			key = '\'',
+			key = 'k',
 			mods = 'CTRL',
 			action = wezterm.action.ClearScrollback 'ScrollbackAndViewport',
 		},
